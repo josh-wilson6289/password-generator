91892684bbda86e051eb783026c39c91b9c64a1b
Josh Wilson <joshua.wilson6289@gmail.com>
2020-08-04 11:47:14 -0500

    got rid of extra else statements

--- Develop/script.js
+++ Develop/script.js
@@ -15,48 +15,43 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // Set arrays for lowercase, uppercase, numbers, and special characters
-var lowercaseString = 'abcdefghijklmnopqrstuvwxyz';
-var uppercaseString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var numbersString = '123456789';
-var specialCharactersString = '!@#$%^&*()_+-='
+  var lowercaseString = 'abcdefghijklmnopqrstuvwxyz';
+  var uppercaseString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbersString = '123456789';
+  var specialCharactersString = '!@#$%^&*()_+-='
 
 // Set passwordString to be the final string to iterate over given user parameters.  
-var passwordString;
+  var passwordString;
 
 // Prompt user for number of characters and whether to include uppercase, numbers, and special characters
-var passwordLength = prompt("Choose amount of characters between 8 and 128.");
-var uppercase = prompt("Include uppercase letters? (y/n)");
-var numbers = prompt("Include numbers? (y/n)");
-var specialCharacters = prompt("Include special characters? (y/n)");
+  var passwordLength = prompt("Choose amount of characters between 8 and 128.");
+  var uppercase = prompt("Include uppercase letters? (y/n)");
+  var numbers = prompt("Include numbers? (y/n)");
+  var specialCharacters = prompt("Include special characters? (y/n)");
 
 // Concatenates arrays for uppercase, numbers, and special characters if user chooses to do so
-if (uppercase === 'y') {
-  passwordString = lowercaseString.concat(uppercaseString);
-}
-else {
-  passwordString = lowercaseString;
+  if (uppercase === 'y') {
+    passwordString = lowercaseString.concat(uppercaseString);
+  }
+  else {
+    passwordString = lowercaseString;
 
-}
+  }
 
-if (numbers === 'y') {
-  passwordString = passwordString.concat(numbersString);
+  if (numbers === 'y') {
+    passwordString = passwordString.concat(numbersString);
 
-}
-else {
-  passwordString = passwordString;
-}
+  }
 
-if (specialCharacters === 'y') {
-  passwordString = passwordString.concat(specialCharactersString);
-}
+  if (specialCharacters === 'y') {
+    passwordString = passwordString.concat(specialCharactersString);
+  }
 
-else {
-  passwordString = passwordString;
-}
+
 
 // Outputs random characters in the array matching the user's inputted password length
-for (i=0; i < passwordLength; i++) {
-  password = passwordString[Math.floor(Math.random() * passwordString.length)];
-  console.log(password);
-}
+  for (i=0; i < passwordLength; i++) {
+    password = passwordString[Math.floor(Math.random() * passwordString.length)];
+    console.log(password);
+  }
 
