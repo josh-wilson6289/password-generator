0269712408920f2997423f26169eb9d721506d0f
Josh Wilson <joshua.wilson6289@gmail.com>
2020-08-04 13:10:16 -0500

    attempt at validation not working

--- Develop/script.js
+++ Develop/script.js
@@ -23,19 +23,37 @@ function generatePassword() {
 // Set passwordString to be the final string to iterate over given user parameters.  
   var passwordString;
 
-// Prompt user for number of characters and whether to include uppercase, numbers, and special characters
+// Prompt user for number of characters and validates response
   var passwordLength = prompt("Choose amount of characters between 8 and 128.");
+    if (Number.isInteger(passwordLength) === false || passwordLength < 8 || passwordLength > 128) {
+      prompt("Please choose a valid password length");
+    }
+  
+// Prompt user to include uppercase letters and validates response
   var uppercase = prompt("Include uppercase letters? (y/n)");
-  var numbers = prompt("Include numbers? (y/n)");
-  var specialCharacters = prompt("Include special characters? (y/n)");
+    if (uppercase.toLowerCase() !== 'y' || uppercase.toLowerCase !== 'n') {
+      prompt("Please choose y or n");
+    }
 
-// Concatenates arrays for uppercase, numbers, and special characters if user chooses to do so
+  // Prompt user to include numbers and validates response  
+  var numbers = prompt("Include numbers? (y/n)");
+    if (numbers.toLowerCase !== 'y' || numbers.toLowerCase !== 'n') {
+    prompt("Please choose y or n");
+    }
+
+// Prompt user to include special characters and validates response
+  var specialCharacters = prompt("Include special characters? (y/n)");
+  specialCharacters.toLowerCase();
+    if (specialCharacters !== 'y' || specialCharacters !== 'n') {
+      prompt("Please choose y or n");
+    }
+
+// Concatenates strings for uppercase, numbers, and special characters if user chooses to do so
   if (uppercase === 'y') {
     passwordString = lowercaseString.concat(uppercaseString);
   }
   else {
     passwordString = lowercaseString;
-
   }
 
   if (numbers === 'y') {
@@ -52,9 +70,7 @@ function generatePassword() {
 // Outputs random characters in the array matching the user's inputted password length
   for (i=0; i < passwordLength; i++) {
     var iteration = passwordString[Math.floor(Math.random() * passwordString.length)];
-    console.log(iteration);
     password = password + iteration;
   }
-  console.log(password);
   return(password);
 }  
